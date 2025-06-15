22f9a7934c05ed32045ef3925167ea984d8333c2
pedro.vasconcelos <pedrohnvasconcelos@gmail.com>
2025-06-14 21:12:10 -0300

    Finish crawl

--- src/main/kotlin/com/nantes/crawler/application/consumer/CrawlerConsumer.kt
+++ src/main/kotlin/com/nantes/crawler/application/consumer/CrawlerConsumer.kt
@@ -1,17 +1,40 @@
 package com.nantes.crawler.application.consumer
 
+import com.nantes.crawler.domain.interfaces.CrawlRepository
+import com.nantes.crawler.domain.models.CrawlStatus
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 import java.util.UUID
 
 
 @Component
-class CrawlerConsumer {
+@Transactional
+class CrawlerConsumer(private val crawlRepository: CrawlRepository) {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
-    @RabbitListener(queues =  ["\${queue.order.name}"] )
-    fun receiveCrawl(id: UUID){
-        logger.info("Message received: $id");
+    @RabbitListener(queues = ["\${queue.order.name}"])
+    fun receiveCrawl(id: String) {
+        runCatching {
+            UUID.fromString(id)
+        }.map { crawlId ->
+                crawlRepository.findById(crawlId) ?: throw IllegalArgumentException("Error")
+            }.map { crawl ->
+                if (crawl.status == CrawlStatus.FINISHED) {
+                    throw IllegalArgumentException("Error")
+                }
+                crawl
+            }.map { crawlToUpdate ->
+                crawlToUpdate.finish()
+            }.onSuccess { updatedCrawl ->
+                crawlRepository.save(updatedCrawl)
+                logger.info("Crawl ${updatedCrawl.id} successfully updated to FINISHED.")
+            }.onFailure { error ->
+                when (error) {
+                    is IllegalArgumentException -> logger.error("Invalid UUID format for ID: $id", error)
+                    else -> logger.error("An unexpected error occurred while processing crawl ID: $id", error)
+                }
+            }
     }
 }
